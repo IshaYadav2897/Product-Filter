a09b8fff43d875802bba7e2988597486cd78be36
IshaYadav2897 <ishayadavie21@gmail.com>
2022-06-14 12:07:13 +0530

    modified exception handler

--- src/main/java/com/flipkart/controller/RestExceptionHandler.java
+++ src/main/java/com/flipkart/controller/RestExceptionHandler.java
@@ -39,20 +39,4 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		Map<String, Object> body = new LinkedHashMap<>();
-		body.put("timestamp", new Date());
-		body.put("status", status.value());
-
-		// Get all errors
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
-				.collect(Collectors.toList());
-
-		body.put("errors", errors);
-
-		return new ResponseEntity<>(body, headers, status);
-	}
 }
